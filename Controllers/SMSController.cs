d98c5f65354675c2379b158560b3f93528f8740e
Stanislav Mozolevskiy <42652969+Stanmozolevskiy@users.noreply.github.com>
2022-06-23 11:05:18 -0400

    Update SMSController.cs

--- Controllers/SMSController.cs
+++ Controllers/SMSController.cs
@@ -20,56 +20,56 @@ namespace Utility.Controllers
 			this.configuration = configuration;
 			accountSid = configuration["Settings:Tvilio:accountSid"];
 			authToken = configuration["Settings:Tvilio:authToken"];
-		}
+	}
 
-		[HttpPost("saveAttachment")]
-		public async Task<IActionResult> SaveAttachment(List<IFormFile> files)
-		{
-			foreach (IFormFile file in files)
-				await Helpers.FileHelper.CopyFileLocally(file.OpenReadStream(),
-					$"{System.IO.Directory.CreateDirectory("Attachments").Name}/{file.FileName}");
+	[HttpPost("saveAttachment")]
+	public async Task<IActionResult> SaveAttachment(List<IFormFile> files)
+	{
+		foreach (IFormFile file in files)
+			await Helpers.FileHelper.CopyFileLocally(file.OpenReadStream(),
+				$"{System.IO.Directory.CreateDirectory("Attachments").Name}/{file.FileName}");
 
-			return Ok();
-		}
+		return Ok();
+	}
 
-		[HttpPost("rempveAttachment")]
-		public async Task<IActionResult> RempveAttachment(IFormCollection fileNames)=>
-			Ok(await Helpers.FileHelper.DeleteFileLocally(fileNames["fileNames"].Single()));
+	[HttpPost("rempveAttachment")]
+	public async Task<IActionResult> RempveAttachment(IFormCollection fileNames)=>
+		Ok(await Helpers.FileHelper.DeleteFileLocally(fileNames["fileNames"].Single()));
 		
-		[HttpPost("send")]
+	[HttpPost("send")]
         public async Task<IActionResult> SendSMS(SMSMessage sms)
         {
-			FirebaseAuthLink firebaseAuthLink = await getFirebaseAuthenticationLink(configuration);
+		FirebaseAuthLink firebaseAuthLink = await getFirebaseAuthenticationLink(configuration);
 
-            TwilioClient.Init(accountSid, authToken);
+		TwilioClient.Init(accountSid, authToken);
 			sms.MediaUrl = await Helpers.FileHelper.UploadAttachmentsToFirebase(configuration, firebaseAuthLink);
             
-			try
-            {
-				await SMS.Send(new NetworkCredential(accountSid, authToken, configuration["Settings:Tvilio:domain"]), sms);
-            }
-			finally
-			{
-				await Helpers.FileHelper.CleanAttachmentsFoldre(configuration, firebaseAuthLink);
-			}
-
-			return Ok();
-        }
-
-		[HttpGet]
-		public async Task<IActionResult> GetAccounts()
+		try
+            	{
+		await SMS.Send(new NetworkCredential(accountSid, authToken, configuration["Settings:Tvilio:domain"]), sms);
+            	}
+		finally
 		{
-			await Task.Yield();
-			return Ok(SMS.GetAccounts(new NetworkCredential(accountSid, authToken, configuration["Settings:Tvilio:domain"])));
+		await Helpers.FileHelper.CleanAttachmentsFoldre(configuration, firebaseAuthLink);
+		}
+
+		return Ok();
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetAccounts()
+	{
+		await Task.Yield();
+		return Ok(SMS.GetAccounts(new NetworkCredential(accountSid, authToken, configuration["Settings:Tvilio:domain"])));
         }
 
 		
-		private readonly string accountSid;
-		private readonly string authToken;
-		private readonly IConfiguration configuration;
-		private async static Task<FirebaseAuthLink> getFirebaseAuthenticationLink(IConfiguration configuration) =>
-			await new FirebaseAuthProvider(new FirebaseConfig(configuration["Settings:Firebase:apiKey"])).SignInWithEmailAndPasswordAsync(
-				configuration["Settings:Firebase:authEmail"], configuration["Settings:Firebase:authPassword"]);
+	private readonly string accountSid;
+	private readonly string authToken;
+	private readonly IConfiguration configuration;
+	private async static Task<FirebaseAuthLink> getFirebaseAuthenticationLink(IConfiguration configuration) =>
+		await new FirebaseAuthProvider(new FirebaseConfig(configuration["Settings:Firebase:apiKey"])).SignInWithEmailAndPasswordAsync(
+			configuration["Settings:Firebase:authEmail"], configuration["Settings:Firebase:authPassword"]);
 
 	}
 }
